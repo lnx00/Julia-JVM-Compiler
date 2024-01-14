e7166fbb0ecd881a4f0cb4554672f1b4c6fb4312
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-14 17:34:23 +0100

    Partial function definition translation

--- Compiler/Core/AST/FunctionDefinitionNode.cs
+++ Compiler/Core/AST/FunctionDefinitionNode.cs
@@ -18,6 +18,37 @@ public class FunctionDefinitionNode : INode
 
     public override List<string> Translate()
     {
-        throw new NotImplementedException();
+        List<string> instructions = new();
+        
+        // Method header
+        // Special case for main | TODO: Don't hardcode this
+        if (Symbol.Name == "main")
+        {
+            instructions.Add(".method public static main([Ljava/lang/String;)V");
+        }
+        else
+        {
+            // TODO: Add parameters
+            instructions.Add($".method public static {Symbol.Name}()V");
+        }
+        
+        // Method prologue
+        // TODO: Calculate stack size
+        instructions.Add(".limit stack 100");
+        instructions.Add(".limit locals 100");
+        
+        // Method body
+        instructions.AddRange(Block.Translate());
+
+        // Method epilogue
+        if (Symbol.Name == "main" || Symbol.Type == TypeManager.DataType.Void)
+        {
+            instructions.Add("return");
+        }
+        
+        // Method end
+        instructions.Add(".end method");
+        
+        return instructions;
     }
 }
