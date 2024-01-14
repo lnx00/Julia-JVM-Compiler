410f1a9928bed596b3df73e47a88cd7662520564
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-14 18:13:16 +0100

    Added jasmin file template

--- Compiler/CodeGenerator/CodeGenerator.cs
+++ Compiler/CodeGenerator/CodeGenerator.cs
@@ -7,6 +7,42 @@ public class CodeGenerator
 {
     public List<string> Generate(BlockNode ast)
     {
-        return ast.Translate();
+        List<string> instructions = new();
+        
+        // Header | TODO: Use source file name
+        instructions.AddRange(new List<string>
+        {
+            ".class public Program",
+            ".super java/lang/Object",
+            string.Empty,
+        });
+        
+        // Initializer
+        instructions.AddRange(new List<string>
+        {
+            ".method public <init>()V",
+            "\taload_0",
+            "\tinvokespecial java/lang/Object/<init>()V",
+            "\treturn",
+            ".end method",
+            string.Empty,
+        });
+        
+        // Source code
+        instructions.AddRange(ast.Translate());
+        
+        // Main method
+        instructions.AddRange(new List<string>
+        {
+            ".method public static main([Ljava/lang/String;)V",
+            "\t.limit stack 100",
+            "\t.limit locals 100",
+            "\tinvokestatic Program/_main()V",
+            "\treturn",
+            ".end method",
+            string.Empty,
+        });
+        
+        return instructions;
     }
 }
