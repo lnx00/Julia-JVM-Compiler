421fa6bbbfe3cb17caf55f8f74c04e38daee3230
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-15 19:11:02 +0100

    Translate AddExpressionNode

--- Compiler/Core/AST/AddExpressionNode.cs
+++ Compiler/Core/AST/AddExpressionNode.cs
@@ -25,6 +25,49 @@ public class AddExpressionNode : ExpressionNode
 
     public override List<string> Translate()
     {
-        throw new NotImplementedException();
+        List<string> instructions = new();
+        
+        instructions.AddRange(LeftExpression.Translate());
+        instructions.AddRange(RightExpression.Translate());
+        
+        switch (OperationType)
+        {
+            case Operation.Add:
+                switch (Type)
+                {
+                    case TypeManager.DataType.Integer:
+                        instructions.Add("\tiadd");
+                        break;
+                    
+                    case TypeManager.DataType.Float64:
+                        instructions.Add("\tfadd");
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                break;
+            
+            case Operation.Subtract:
+                switch (Type)
+                {
+                    case TypeManager.DataType.Integer:
+                        instructions.Add("\tisub");
+                        break;
+                    
+                    case TypeManager.DataType.Float64:
+                        instructions.Add("\tfsub");
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return instructions;
     }
 }
