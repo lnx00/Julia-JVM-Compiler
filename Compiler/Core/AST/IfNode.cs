2c0dfc1ba17b2f4bd3c14b6b149c5d8828ca92b8
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-16 18:10:22 +0100

    Added basic if statements

--- Compiler/Core/AST/IfNode.cs
+++ Compiler/Core/AST/IfNode.cs
@@ -1,4 +1,6 @@
-﻿namespace Compiler.Core.AST;
+﻿using Compiler.Core.Common;
+
+namespace Compiler.Core.AST;
 
 public class IfNode : INode
 {
@@ -15,6 +17,36 @@ public class IfNode : INode
 
     public override List<string> Translate()
     {
-        throw new NotImplementedException();
+        List<string> instructions = new() { "\t; If statement" };
+
+        // Translate condition
+        instructions.AddRange(Condition.Translate());
+
+        // Create labels
+        string elseLabel = LabelManager.GetLabel("else");
+        string endLabel = LabelManager.GetLabel("end");
+
+        // Compare condition to 0
+        instructions.Add("\tifeq " + elseLabel);
+
+        // Translate body
+        instructions.AddRange(Body.Translate());
+
+        // Jump to end
+        instructions.Add("\tgoto " + endLabel);
+
+        // Else label
+        instructions.Add(elseLabel + ":");
+
+        // Translate else body
+        if (ElseBody is not null)
+        {
+            instructions.AddRange(ElseBody.Translate());
+        }
+
+        // End label
+        instructions.Add(endLabel + ":");
+
+        return instructions;
     }
 }
---
+++ Compiler/Core/Common/LabelManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Compiler.Core.Common;
+
+public static class LabelManager
+{
+    private static int _labelCounter = 0;
+    
+    public static string GetLabel()
+    {
+        return $"L{_labelCounter++}";
+    }
+    
+public static string GetLabel(string label)
+    {
+        return $"{label}{_labelCounter++}";
+    }
+}
