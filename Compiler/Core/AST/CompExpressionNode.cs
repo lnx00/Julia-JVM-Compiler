9c3eaeed8c5373e64eff4893f167a91f7cc1c3e6
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-16 18:28:17 +0100

    Added hacky comparison translator

--- Compiler/Core/AST/CompExpressionNode.cs
+++ Compiler/Core/AST/CompExpressionNode.cs
@@ -28,6 +28,50 @@ public class CompExpressionNode : ExpressionNode
 
     public override List<string> Translate()
     {
-        throw new NotImplementedException();
+        List<string> instructions = new();
+
+        instructions.AddRange(LeftExpression.Translate());
+        instructions.AddRange(RightExpression.Translate());
+        
+        string trueLabel = LabelManager.GetLabel("compTrue");
+        string endLabel = LabelManager.GetLabel("compEnd");
+
+        switch (OperationType)
+        {
+            case Operation.Equal:
+                instructions.Add("\tif_icmpeq " + trueLabel);
+                break;
+            
+            case Operation.NotEqual:
+                instructions.Add("\tif_icmpne " + trueLabel);
+                break;
+            
+            case Operation.LessThan:
+                instructions.Add("\tif_icmplt " + trueLabel);
+                break;
+            
+            case Operation.LessThanOrEqual:
+                instructions.Add("\tif_icmple " + trueLabel);
+                break;
+            
+            case Operation.GreaterThan:
+                instructions.Add("\tif_icmpgt " + trueLabel);
+                break;
+            
+            case Operation.GreaterThanOrEqual:
+                instructions.Add("\tif_icmpge " + trueLabel);
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        instructions.Add("\ticonst_0");
+        instructions.Add("\tgoto " + endLabel);
+        instructions.Add(trueLabel + ":");
+        instructions.Add("\ticonst_1");
+        instructions.Add(endLabel + ":");
+
+        return instructions;
     }
 }
