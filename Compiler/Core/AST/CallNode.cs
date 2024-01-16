2ef8ff0442a9bb39744e856fdb1af8187c0211dc
lnx00 <34384416+lnx00@users.noreply.github.com>
2024-01-16 17:02:10 +0100

    Translate call node

--- Compiler/Core/AST/CallNode.cs
+++ Compiler/Core/AST/CallNode.cs
@@ -18,6 +18,18 @@ public class CallNode : ExpressionNode
 
     public override List<string> Translate()
     {
-        throw new NotImplementedException();
+        List<string> instructions = new();
+        
+        // Push arguments to stack
+        foreach (var argument in Arguments)
+        {
+            instructions.AddRange(argument.Translate());
+        }
+
+        var parameterTypes = Arguments.Aggregate(string.Empty, (current, arg) => current + TypeManager.GetJasminType(arg.Type));
+        var returnType = TypeManager.GetJasminType(Type);
+        instructions.Add($"\tinvokestatic Program/{Symbol.GetMangledName()}({parameterTypes}){returnType}");
+
+        return instructions;
     }
 }
